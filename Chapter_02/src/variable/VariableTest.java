d4e600aa6e9a33a6a352babb8ae9943e38015f26
SupremeYE <nina7963@naver.com>
2020-03-09 15:09:54 +0900

    변수선언/ 값대입하기

---
+++ Chapter_02/src/variable/VariableTest.java
@@ -0,0 +1,20 @@
+package variable;//패키지는 소문자로
+
+public class VariableTest { //클래스는 대문자로시작하고 단어가바뀔때 다시 대문자로
+
+	public static void main(String[] args) {//메인코드
+		int age, count;//변수선언
+		
+		age=10;//변수에 값 대입하기
+		
+		System.out.println(age);
+		
+		int numberOfStudent=20;
+		
+		
+		System.out.println(numberOfStudent);
+		
+		
+	}
+
+}
