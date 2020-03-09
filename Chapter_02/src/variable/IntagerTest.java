35d51f585a716872f37d71475151266a6897e113
SupremeYE <nina7963@naver.com>
2020-03-09 17:33:37 +0900

    정수자료형

---
+++ Chapter_02/src/variable/IntagerTest.java
@@ -0,0 +1,19 @@
+package variable;
+
+public class IntagerTest {
+	
+	public static void main(String[] args) {
+		
+		byte bs = -127;
+		System.out.println(bs);
+		
+		//byte= bs2 = 127;
+		//System.out.println(bs2);
+		
+		
+		//int iVal = 12345678900;
+		
+		  long lVal = 12345678900L; //숫자자체를 4바이트로 둠 자바는 기본적으로 숫자를 정수형에 넣음
+	}
+
+}
