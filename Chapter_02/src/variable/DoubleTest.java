5ff87915a5b4e7ec6d73b9c49a03b97a3f0cf881
SupremeYE <nina7963@naver.com>
2020-03-14 13:18:56 +0900

    Double 자료형

---
+++ Chapter_02/src/variable/DoubleTest.java
@@ -0,0 +1,17 @@
+package variable;
+
+public class DoubleTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		double dNume = 3.14;
+		
+		float fNum = 3.14f;//내부적으로 float를 식별하기위해 f를 붙여야함
+		
+		System.out.println(dNume);
+		System.out.println(fNum);
+	}
+
+}
